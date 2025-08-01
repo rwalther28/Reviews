81fe023fc0323b8436d908c858bbef6353ae1bd8
Reese <reesemiyowalther@gmail.com>
2025-07-31 14:12:22 -1000

    Initial Commit

---
+++ Reviews/ReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewsApp.swift
+//  Reviews
+//
+//  Created by rwalther28 on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct ReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
